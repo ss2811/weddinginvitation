f0c4ed3dd591c2527bf659caefda9df40a43d6a0
ss2811 <ygxx13@gmail.com>
2025-09-02 07:29:55 +0800

    Update enhanced-app.js

--- enhanced-app.js
+++ enhanced-app.js
@@ -1003,7 +1003,7 @@ END:VCALENDAR`;
 }
 
 async function shareInvitation() {
-    const shareText = 'Assalamualaikum Wr. Wb.\nDengan penuh syukur kepada Allah SWT, kami mengundang Bapak/Ibu/Saudara(i) menghadiri pernikahan Suriansyah & Sonia. Barakallahu lakuma wa baraka 'alaikuma.';
+    const shareText = 'Assalamualaikum Wr. Wb.\nDengan penuh syukur kepada Allah SWT, kami mengundang Bapak/Ibu/Saudara(i) menghadiri pernikahan Suriansyah & Sonia. Barakallahu lakuma wa baraka alaikuma.';
     const shareTitle = 'Undangan Pernikahan | Suriansyah & Sonia';
     const shareUrl = window.location.href;
     const imageUrl = 'https://raw.githubusercontent.com/ss2811/weddinginvitation/main/pemisah.jpeg';
